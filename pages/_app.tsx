da31934311bd1486527b3cd2c0b40d08c42b20dd
geeky-vaiiib <geeky.vaiiib28@gmail.com>
2025-12-04 22:14:18 +0530

    chore: add Next _app to load global styles and providers

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,21 @@
 import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
 import "../styles/globals.css";
 
+const queryClient = new QueryClient();
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Component {...pageProps} />
+        <Toaster />
+        <Sonner />
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
 }
 
